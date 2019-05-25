90f5fd899018bf87671b3d86ccb4759266dfe76b
pinzolo <pinzolo@gmail.com>
2019-05-25 18:22:01 +0900

    Add to top button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,21 @@
 var navbarBurger = document.getElementById('navbar-burger');
 navbarBurger.addEventListener(
   'click',
-  function() {
+  function(e) {
+    e.preventDefault();
     navbarBurger.classList.toggle('is-active');
     var navbarLinks = document.getElementById('navbar-links');
     navbarLinks.classList.toggle('is-active');
+  },
+  false
+);
+
+var toTopButton = document.getElementById('to-top');
+toTopButton.addEventListener(
+  'click',
+  function(e) {
+    e.preventDefault();
+    Jump('#top', { duration: 500 });
   },
   false
 );
