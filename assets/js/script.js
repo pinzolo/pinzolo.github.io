de4f65f341b6552571c525fe08faab965c15367d
pinzolo <pinzolo@gmail.com>
2019-06-15 01:51:42 +0900

    Wrap by DOMContentLoaded event

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,25 @@
-var navbarBurger = document.getElementById('navbar-burger');
-navbarBurger.addEventListener(
-  'click',
-  e => {
-    e.preventDefault();
-    navbarBurger.classList.toggle('is-active');
-    var navbarLinks = document.getElementById('navbar-links');
-    navbarLinks.classList.toggle('is-active');
-  },
-  false
-);
-
-var toTopButton = document.querySelectorAll('.to-top-button');
-toTopButton.forEach(el => {
-  el.addEventListener(
-    'click',
+document.addEventListener("DOMContentLoaded", () => {
+  var navbarBurger = document.getElementById("navbar-burger");
+  navbarBurger.addEventListener(
+    "click",
     e => {
       e.preventDefault();
-      Jump('#top', { duration: 500 });
+      navbarBurger.classList.toggle("is-active");
+      var navbarLinks = document.getElementById("navbar-links");
+      navbarLinks.classList.toggle("is-active");
     },
     false
   );
+
+  var toTopButton = document.querySelectorAll(".to-top-button");
+  toTopButton.forEach(el => {
+    el.addEventListener(
+      "click",
+      e => {
+        e.preventDefault();
+        Jump("#top", { duration: 500 });
+      },
+      false
+    );
+  });
 });
