b2f2732b3085de9b5ff034d78944565739982ce1
pinzolo <pinzolo@gmail.com>
2022-03-08 11:18:09 +0900

    Use const instead of var

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  var navbarBurger = document.getElementById("navbar-burger");
+  const navbarBurger = document.getElementById("navbar-burger");
   navbarBurger.addEventListener(
     "click",
     e => {
       e.preventDefault();
       navbarBurger.classList.toggle("is-active");
-      var navbarLinks = document.getElementById("navbar-links");
+      const navbarLinks = document.getElementById("navbar-links");
       navbarLinks.classList.toggle("is-active");
     },
     false
   );
 
-  var toTopButton = document.querySelectorAll(".to-top-button");
+  const toTopButton = document.querySelectorAll(".to-top-button");
   toTopButton.forEach(el => {
     el.addEventListener(
       "click",
@@ -24,14 +24,13 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   dayjs.locale("ja");
-  var postTimeElms = document.querySelectorAll(".post-time");
+  const postTimeElms = document.querySelectorAll(".post-time");
   postTimeElms.forEach(el => {
     el.innerHTML = dayjs(el.dataset["postTime"]).format(el.dataset["timeFormat"]);
   });
 
-  var images = document.getElementsByTagName("img");
+  const images = document.getElementsByTagName("img");
   Array.prototype.forEach.call(images, img => {
-    console.log(img);
     if (img.parentNode.tagName === "P") {
       img.parentNode.classList.add("img-container");
     }
